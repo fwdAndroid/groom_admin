d3fd06ef2ac12cb74371c93b405e52a1b4b698b9
fwdAndroid <fwdkaleem@gmail.com>
2024-04-18 04:07:26 +0500

    Main Screen Added

---
+++ lib/screens/nav_screens/provider_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProviderScreen extends StatefulWidget {
+  const ProviderScreen({super.key});
+
+  @override
+  State<ProviderScreen> createState() => _ProviderScreenState();
+}
+
+class _ProviderScreenState extends State<ProviderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
