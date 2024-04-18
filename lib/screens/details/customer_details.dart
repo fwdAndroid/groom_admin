164a3cb215f72ae1da535aef3e2e27a71f84e58c
fwdAndroid <fwdkaleem@gmail.com>
2024-04-18 05:04:01 +0500

    Customer Section Done

---
+++ lib/screens/details/customer_details.dart
@@ -0,0 +1,208 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:groom_admin/screens/home_page.dart';
+import 'package:groom_admin/utils/buttons.dart';
+
+class CustomerDetails extends StatefulWidget {
+  final uid, email, fullName, password, contactNumber;
+  CustomerDetails(
+      {super.key,
+      required this.contactNumber,
+      required this.email,
+      required this.fullName,
+      required this.password,
+      required this.uid});
+
+  @override
+  State<CustomerDetails> createState() => _CustomerDetailsState();
+}
+
+class _CustomerDetailsState extends State<CustomerDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                FormSection(
+                  contactNumber: widget.contactNumber,
+                  email: widget.email,
+                  fullName: widget.fullName,
+                  password: widget.password,
+                  uid: widget.uid,
+                ),
+                _ImageSection(),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class FormSection extends StatefulWidget {
+  final contactNumber;
+  final email;
+  final uid;
+  final fullName;
+  final password;
+
+  const FormSection({
+    Key? key,
+    required this.password,
+    required this.email,
+    required this.fullName,
+    required this.contactNumber,
+    required this.uid,
+  }) : super(key: key);
+
+  @override
+  State<FormSection> createState() => FormSectionState();
+}
+
+class FormSectionState extends State<FormSection> {
+  //Program
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+          width: 448,
+          padding: const EdgeInsets.symmetric(horizontal: 50),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const Text(
+              "Customer Detail",
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 25.63),
+            ),
+            SizedBox(height: 9),
+            Row(children: [
+              Text(
+                "Name:",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Text(widget.fullName)
+            ]),
+            SizedBox(height: 9),
+            Row(children: [
+              Text(
+                "Email:",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Text(
+                widget.email,
+              )
+            ]),
+            SizedBox(height: 9),
+            Row(children: [
+              Text(
+                "Password:",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Text(widget.password),
+            ]),
+            SizedBox(height: 9),
+            Row(children: [
+              Text(
+                "Contact Number:",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Text(
+                widget.contactNumber,
+              )
+            ]),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: SaveButton(
+                    onTap: () async {
+                      showDialog<void>(
+                        context: context,
+                        barrierDismissible: false, // user must tap button!
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('Staff Detail'),
+                            content: SingleChildScrollView(
+                              child: ListBody(
+                                children: <Widget>[
+                                  Text("Email :" + widget.email),
+                                  Text("Name :" + widget.fullName),
+                                  Text(
+                                    "Do you want to remove user account permantely",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                            ),
+                            actions: <Widget>[
+                              TextButton(
+                                child: const Text('Yes'),
+                                onPressed: () async {
+                                  await FirebaseFirestore.instance
+                                      .collection("customer")
+                                      .doc(widget.uid)
+                                      .delete();
+
+                                  Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (builder) => HomePage()));
+                                },
+                              ),
+                              TextButton(
+                                child: const Text('No'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    title: "Delete Account"),
+              ),
+            ),
+            SizedBox(height: 10),
+          ])),
+    );
+  }
+}
+
+class _ImageSection extends StatelessWidget {
+  const _ImageSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+              child: Image.asset(
+            "assets/logo.png",
+            height: 300,
+          ))
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/nav_screens/customer_screen.dart
+++ lib/screens/nav_screens/customer_screen.dart
@@ -1,4 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:groom_admin/screens/details/customer_details.dart';
+import 'package:groom_admin/utils/buttons.dart';
+import 'package:groom_admin/utils/colors.dart';
 
 class CustomerScreen extends StatefulWidget {
   const CustomerScreen({super.key});
@@ -8,8 +14,249 @@ class CustomerScreen extends StatefulWidget {
 }
 
 class _CustomerScreenState extends State<CustomerScreen> {
+  TextEditingController controller = TextEditingController();
+  bool isShowUser = false;
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+          leading: Image.asset("assets/logo.png"),
+          elevation: 0,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    isShowUser = true;
+                  });
+                },
+                icon: Icon(Icons.search))
+          ],
+          iconTheme: IconThemeData(color: Colors.black),
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: TextFormField(
+            controller: controller,
+            decoration: InputDecoration(label: Text('Search By Name')),
+            onFieldSubmitted: (_) {
+              setState(() {
+                isShowUser = true;
+              });
+            },
+          )),
+      body: isShowUser
+          ? StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection("customer")
+                  .where("fullName", isGreaterThanOrEqualTo: controller.text)
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Text('Loading...');
+                }
+
+                List<DocumentSnapshot> documents = snapshot.data!.docs;
+                return Container(
+                  height: 400,
+                  child: ListView.builder(
+                    itemCount: documents.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      var documentData =
+                          documents[index].data() as Map<String, dynamic>;
+                      return ListTile(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (builder) => CustomerDetails(
+                                        email: documentData['email'],
+                                        fullName: documentData['fullName'],
+                                        uid: documentData['uid'],
+                                        contactNumber:
+                                            documentData['contactNumber'],
+                                        password: documentData['password'],
+                                      )));
+                        },
+                        title: Text(
+                          documents[index]['fullName'],
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        subtitle: Text(
+                          documents[index]['email'],
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+            )
+          : StreamBuilder(
+              stream:
+                  FirebaseFirestore.instance.collection("customer").snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                var data = snapshot.data!.docs;
+                if (data.isEmpty) {
+                  // No records found
+                  return Center(
+                    child: Text('Currently No User Available in Our System'),
+                  );
+                }
+                return LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    // Calculate the number of columns based on available width
+                    int columns = (constraints.maxWidth / 250)
+                        .floor(); // Assuming each item has a width of 200
+
+                    return GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: columns,
+                        crossAxisSpacing: 8.0,
+                        mainAxisSpacing: 8.0,
+                      ),
+                      itemCount: data.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        var documentData =
+                            data[index].data() as Map<String, dynamic>;
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12)),
+                            color: mainBtnColor,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(
+                                      "Email: ",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 8.0, right: 8),
+                                  child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(
+                                      documentData['email'],
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 7,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(
+                                      "Name: ",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 8.0, right: 8),
+                                  child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(
+                                      documentData['fullName'],
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 7,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(
+                                      "Mobile: ",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 8.0, right: 8),
+                                  child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(
+                                      documentData['contactNumber'],
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: SaveButton(
+                                      onTap: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (builder) =>
+                                                    CustomerDetails(
+                                                      email:
+                                                          documentData['email'],
+                                                      fullName: documentData[
+                                                          'fullName'],
+                                                      uid: documentData['uid'],
+                                                      contactNumber:
+                                                          documentData[
+                                                              'contactNumber'],
+                                                      password: documentData[
+                                                          'password'],
+                                                    )));
+                                      },
+                                      title: "View Detail"),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                );
+              }),
+    );
   }
 }
